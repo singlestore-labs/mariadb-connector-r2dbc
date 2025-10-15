0b6ad26ddc7d1d8b053591dc265fde8aab1585fc
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2025-10-15 13:44:04 +0300

    Changed to update mtls config only for 9.0

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -96,9 +96,11 @@ fi
 echo
 echo "Setting up SSL"
 docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key ssl_ca --value /test-ssl/ca-cert.pem
-docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key ssl_ca_for_client_cert --value /test-ssl/ca-cert.pem
 docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key ssl_cert --value /test-ssl/server-cert.pem
 docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key ssl_key --value /test-ssl/server-key.pem
+if (( $(echo "$VERSION >= 9.0" | bc -l) )); then
+  docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key ssl_ca_for_client_cert --value /test-ssl/ca-cert.pem
+fi
 echo "Setting up JWT"
 docker exec ${CONTAINER_NAME} memsqlctl update-config --yes --all --key jwt_auth_config_file --value /test-jwt/jwt_auth_config.json
 echo "Restarting cluster"
