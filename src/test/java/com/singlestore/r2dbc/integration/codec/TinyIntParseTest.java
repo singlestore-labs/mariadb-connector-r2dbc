85f788de8e302f7803934d1eebfe57e3de207da4
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2025-11-06 11:41:21 +0200

    Added mariadb copyright

--- src/test/java/com/singlestore/r2dbc/integration/codec/TinyIntParseTest.java
+++ src/test/java/com/singlestore/r2dbc/integration/codec/TinyIntParseTest.java
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2020-2024 MariaDB Corporation Ab
 // Copyright (c) 2025-2025 SingleStore, Inc.
 
 package com.singlestore.r2dbc.integration.codec;
